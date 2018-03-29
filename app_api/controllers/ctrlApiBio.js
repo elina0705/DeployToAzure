456b8a1da4828b24bd88a94e4aa5ea185c46b64e
elina0705 <shippai.da@gmail.com>
2018-03-29 10:34:53 +0300

    API testing 2

--- app_api/controllers/ctrlApiBio.js
+++ app_api/controllers/ctrlApiBio.js
@@ -2,13 +2,24 @@ const mongoose = require('mongoose');
 const bioModel = mongoose.model('bio');
 
 const factList = function (req, res){
-    res
-        .status(200)
-        .json({"fact": "fact2"});
+    bioModel.find({}, function(err, facts){
+        if(err){
+        res.status(404).json(err);
+        }
+    else{
+        res.status(200).json(facts);
+        }
+});
 };
-
 const addFact = function (req, res){
-    res.status(201).json({"Add fact" : "Fact"});
+    bioModel.create(req.body, function(err, newFact){
+        if(err){
+            res.status(400).json(err);
+        }
+        else{
+            res.status(201).json(newFact);
+        }
+    });
 };
 
 module.exports = {
--- app_api/controllers/ctrlApiDisco.js
+++ app_api/controllers/ctrlApiDisco.js
@@ -2,13 +2,25 @@ const mongoose = require('mongoose');
 const discoModel = mongoose.model('discography');
 
 const discoList = function (req, res){
-    res
-        .status(200)
-        .json({"year": "release"});
+    discoModel.find({}, function(err, records){
+    if(err){
+        res.status(404).json(err);
+    }
+    else{
+        res.status(200).json(records);
+    }
+});
 };
 
 const addRecord = function (req, res){
-    res.status(201).json({"Add release" : "Record"});
+    discoModel.create(req.body, function(err, newRecord){
+        if(err){
+            res.status(400).json(err);
+        }
+        else{
+            res.status(201).json(newRecord);
+        }
+    });
 };
 
 module.exports = {
