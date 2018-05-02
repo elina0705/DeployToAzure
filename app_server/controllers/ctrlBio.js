1a2c8e5d8fdee06520639e0881be319ed26e33a2
elina0705 <shippai.da@gmail.com>
2018-05-02 20:42:55 +0300

    GET

--- app_server/controllers/ctrlBio.js
+++ app_server/controllers/ctrlBio.js
@@ -1,6 +1,8 @@
-/* GET home page */
+const request = require('request');
+const apiURL = require('./apiURLs');
+
 const contain = function (req, res) {
-    res.render('bio', {
+ /*  res.render('bio', {
         facts:
             [
                 {fact: 'Matsumoto Hideto'},
@@ -8,7 +10,31 @@ const contain = function (req, res) {
                 {fact: 'Guitarist in X Japan'},
 
             ]
-    });
+    });*/
+ const path = '/api/bio';
+ const requestOptions = {
+     url : apiURL.server + path,
+     method : 'GET',
+     json: {},
+     qs : {}
+ };
+ request(
+     requestOptions,
+     function (err, response, body){
+         if (err){
+             res.render('error', {message: err.message});
+         } else if (response.statusCode !== 200){
+             res.render('error', {message: 'Error accessing API: ' + response.statusMessage + " (" +response.statusCOde + ")"});
+         } else if (!(body instanceof Array)) {
+             res.render('error', {message: 'Unexpected response data'});
+         } else if (!body.length){
+             res.render('error', {message: 'No documents in collection'});
+         } else {
+             res.render('bio', {facts: body});
+         }
+
+     }
+ )
 };
 module.exports = {
     contain
